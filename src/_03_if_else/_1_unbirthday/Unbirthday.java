7880e0a3a98741abdef4b04bc6e2aaebbc1e48df
league <league@wts14-3.attlocal.net>
2022-10-07 18:26:15 -0700

    i did the venn diagram, the unbirthday thing, and the remarkable thing. it was very fun!

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,58 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "hello and welcome!");
+		String bday= JOptionPane.showInputDialog("when is your birthday?\n" +
+									"use this format: Month/Day/Year");
+		
+		if (bday.equals("10/7/2022")){
+			JOptionPane.showMessageDialog(null, "today's your lucky day! well, i wish you a very happy birthday!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "*ha ha* i wish you a very happy UNbirthday!!!");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	}
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -2,17 +2,53 @@ package _03_if_else._2_remarkable;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class Remarkable {
 public static void main(String[] args) {
 
+	
+	String name= JOptionPane.showInputDialog("what is your name?");
+	
+	String re= " you are very funny!";
+    String marka="you are very kind!";
+    String ble="you are very brave!";
+    String ha="you are very smart!";
+    String boo="you are very strong!";
+    
+	Random thing=new Random();
+	int weird=thing.nextInt(5);
+	 
+	if (weird==0)  {
+		JOptionPane.showMessageDialog(null,name+","+ re);
+	}
+	
+	if (weird==1)  {
+		JOptionPane.showMessageDialog(null, name+","+ marka);
+	}
+	
+	if (weird==2)  {
+		JOptionPane.showMessageDialog(null, name+","+ ble);
+	}
+	
+	if (weird==3)  {
+		JOptionPane.showMessageDialog(null,name+","+ ha);
+	}
+	
+	if (weird==4)  {
+		JOptionPane.showMessageDialog(null,name+","+ boo);
+	}
+	
+	
+	
 		// 1. Save something remarkable about each person in a variable.
-
+      
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+      
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+  	
 	}
 }
 
